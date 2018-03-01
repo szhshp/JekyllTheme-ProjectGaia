d6f38f4af7c7858510ea1831cacac774a138a4be
szh-laptop-hp <24031shp@sina.com>
2018-03-01 23:05:56 +0800

    Updated: * a more fluent sidebar design

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,12 +1,8 @@
 (function () {
     $("a#menu-toggle").on('click', sidebarToggle);
-    /*$(document).ready(function(){
-        sidebarToggle();
-    })*/
 
     function sidebarToggle(){
-        $("div#wrapper").toggleClass("toggled");
+        $("body").toggleClass("toggled");
         $('a#menu-toggle').find('i.fa').toggleClass('fa-bars').toggleClass('fa-times');
-        $('a#menu-toggle').toggleClass('toggled');
     }
 } ());
